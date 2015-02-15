63a6defbdf8005cf85fa2ac9117abb8a6a44d0ee
LOZORD <ljrudberg@gmail.com>
2015-02-15 11:38:04 -0600

    Started basis for P2

---
+++ p2/mysh.c
@@ -0,0 +1,10 @@
+//mysh, by Leo Rudberg and Nicholas Hyatt
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "mysh.h"
+
+int main (int argc, char ** argv) {
+  printf("Hello and welcome to mysh!\n");
+  exit(EXIT_SUCCESS);
+}
---
+++ p2/mysh.h
@@ -0,0 +1,6 @@
+#ifndef __MYSH_H__
+#define __MYSH_H__
+
+//The header file for mysh
+
+#endif
