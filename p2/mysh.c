fadf9ff12580bbbf99b4c59d884ba9db56c62f11
LOZORD <ljrudberg@gmail.com>
2015-02-17 18:13:09 -0600

    Basic input loop running

--- p2/mysh.c
+++ p2/mysh.c
@@ -4,7 +4,37 @@
 #include <stdio.h>
 #include "mysh.h"
 
+size_t MAX_INPUT_LENGTH = 1024;
+
+extern FILE * stdin;
+extern FILE * stdout;
+extern FILE * stderr;
+
+int getcmd (FILE * file, char * buff) {
+  if (file == NULL || buff == NULL) {
+    return -1;
+  }
+
+  int result;
+
+  printf("mysh> ");
+  result = getline(&buff, &MAX_INPUT_LENGTH, file);
+
+  return result;
+}
+
+
 int main (int argc, char ** argv) {
   printf("Hello and welcome to mysh!\n");
+  //TODO: open tee.txt or whatever
+  //
+
+  char buff [MAX_INPUT_LENGTH + 1];
+
+  while (getcmd(stdin, buff) >= 0) {
+    printf("mysh got: %s\n", buff);
+  }
+
+
   exit(EXIT_SUCCESS);
 }
