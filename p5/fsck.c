ffc364048249bf8b574df192cf5d7a72265ac8db
LOZORD <ljrudberg@gmail.com>
2015-05-11 14:54:49 -0500

    Working on checking inodes and the freemap

--- p5/fsck.c
+++ p5/fsck.c
@@ -12,16 +12,29 @@
 #define VALID 1
 #define INVALID 0
 
-#define DEBUG 0
+#define DEBUG 1
+// File system implementation.  Four layers:
+//   + Blocks: allocator for raw disk blocks.
+//   + Files: inode allocator, reading, writing, metadata.
+//   + Directories: inode with special contents (list of other inodes!)
+//   + Names: paths like /usr/rtm/xv6/fs.c for convenient naming.
+//
+// Disk layout is: superblock, inodes, block in-use bitmap, data blocks.
 
 //Function prototypes
 int checkSuperblock (struct superblock *);
 void reportAndDie (int, char *);
 int seekToBlock (int blockNum);
 int getImageFd (char * filename);
+int checkFreeMap ();
+int getBitIndexFromAddr (int);
+int getCharIndexFromAddr (int);
 //Global data
 int imageFd;
 struct superblock mySuperblock;
+char * theirFreeMap;
+char * myFreeMap;
+struct dinode * myInodes;
 
 int main (int argc, char ** argv) {
   #if DEBUG
@@ -39,6 +52,46 @@ int main (int argc, char ** argv) {
   reportAndDie(checkSuperblock(&mySuperblock), "Superblock corrupted");
   #if DEBUG
   printf("Superblock is ok\n");
+  #endif
+
+  //read in inodes and bitmap
+  int inodeRegionStart  = 1 + 1; //garbage, superblock, inodes
+  seekToBlock(inodeRegionStart);
+  myInodes = malloc(mySuperblock.ninodes * sizeof(struct dinode)); //TODO free
+  read(imageFd, myInodes, mySuperblock.ninodes * sizeof(struct dinode));
+  //garbage, super, inodes, garbage, bitmap
+  int bitMapRegionStart = 1 + 1 + (mySuperblock.ninodes / IPB) + 1;
+
+  #if DEBUG
+  printf("bit map starts at:%x\t%d\n", bitMapRegionStart, bitMapRegionStart);
+  #endif
+
+  seekToBlock(bitMapRegionStart); //seek to bitmap region
+  //sizeof(char); //size of the freemap in char sizes (8 bits = 1 byte = 1 char)
+  int numBlockChars = (mySuperblock.nblocks/8) + (mySuperblock.nblocks % 8 != 0 ? 1 : 0);
+
+  #if DEBUG
+  printf("numBlockChars: %d\tBlock size: %d\n", numBlockChars, BSIZE);
+  #endif
+  theirFreeMap = malloc(numBlockChars);
+  myFreeMap = calloc(numBlockChars, sizeof(char)); //malloc and init to zero
+  //TODO free
+  read(imageFd, theirFreeMap, numBlockChars);
+
+  #if DEBUG
+  int i;
+  for (i = 0; i < numBlockChars; i++) {
+    printf("%x ", theirFreeMap[i]);
+  }
+  #endif
+
+  reportAndDie(checkFreeMap(), "Freemap is not good");
+  #if DEBUG
+  printf("\nOUR BITMAP\n");
+  for (i = 0; i < numBlockChars; i++) {
+    printf("%x ", myFreeMap[i]);
+  }
+  printf("\n");
   #endif
 
   exit(EXIT_SUCCESS);
@@ -93,6 +146,7 @@ int checkSuperblock (struct superblock * super) {
   printf("super\n\tsize\t%d\n\tnblocks\t%d\n\tninodes\t%d\n",
     super->size, super->nblocks, super->ninodes);
   #endif
+  //XXX should these checks come beforehand?
   if (super->size % BSIZE != 0) {
     return INVALID;
   }
@@ -101,28 +155,58 @@ int checkSuperblock (struct superblock * super) {
     return INVALID;
   }
 
+  return VALID;
+}
 
-  /*
-  uint totalINodeSize = super->ninodes * sizeof(struct dinode);
-  uint totalBlockSize = super->nblocks * BSIZE;
+void markAsBusyInMyFreeMap (int addr) {
+  //SHIFTING
+  int charInd = getCharIndexFromAddr(addr);
+  char c = myFreeMap[charInd];
+  int bitInd = getBitIndexFromAddr(addr);
+  char bit = 8 >> bitInd;
+  bit &= c;
+  myFreeMap[charInd] = bit;
+}
 
-  if (  super->size < totalINodeSize ||
-        super->size < totalBlockSize ||
-        super->size < totalINodeSize + totalBlockSize) {
-    return INVALID;
+int getCharIndexFromAddr (int addr) {
+  //garbage + super + inodes + garbage + bitmap
+  int dataBlockRegionStart = (1 + 1 + (mySuperblock.ninodes / IPB) + 1 + 1) * 512;
+  int blockNum = (addr - dataBlockRegionStart) / 512;
+  //assert(addr % 512 == 0);
+  return blockNum / 8;
+}
+int getBitIndexFromAddr (int addr) {
+  //garbage + super + inodes + garbage + bitmap
+  int dataBlockRegionStart = (1 + 1 + (mySuperblock.ninodes / IPB) + 1 + 1) * 512;
+  int blockNum = (addr - dataBlockRegionStart) / 512;
+  //assert(addr % 512 == 0);
+  return blockNum % 8;
+}
+
+void printInode (struct dinode * i) {
+  printf("inode.size:%d\n\t.type:%d\n\t.nlinks:%d\n", i->size, i->type, i->nlink);
+}
+
+int checkFreeMap () {
+  //we need to check that each inode correlates to a block
+
+  int i;
+
+  for (i = 1; i < mySuperblock.ninodes; i++) {
+    printf("myInodes[%d]\n", i);
+    printInode(&myInodes[i]);
+    if(myInodes[i].type > 0) {
+      int j;
+      for (j = 0; j < NDIRECT; j++) {
+        int addr = myInodes[i].addrs[j];
+        markAsBusyInMyFreeMap(addr);
+      }
+      //then do indirect
+    }
+    else {
+      //TODO
+    }
   }
-
-  if (super->size < totalINodeSize + totalBlockSize) {
-    return INVALID;
-  }
-  */
-
-  /*
-  if (super->size < super->nblocks + super->ninodes) {
-    return INVALID;
-  }
-  */
-
   return VALID;
 }
 
